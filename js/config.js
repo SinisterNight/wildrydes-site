671fd4e8ee178f5c1480852d8fae718a9d7f9deb
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-11-19 10:52:08 -0600

    Update config.js
    new config for email

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_4DORmL4b9',         // e.g. us-east-2_4DORmL4b9
-        userPoolClientId:   'fq5t0tnju3b9c14phit21ftn8',         // e.g. fq5t0tnju3b9c14phit21ftn8
+        userPoolId:         'us-east-2_z46p2SNyr',         // e.g. us-east-2_4DORmL4b9
+        userPoolClientId:   '52q044vcelep5jb37mgc2vd222',         // e.g. fq5t0tnju3b9c14phit21ftn8
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
