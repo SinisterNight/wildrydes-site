b3297d709283d595a40f25a28bf2d102c68db677
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-11-19 10:47:19 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_4DORmL4b9
-        userPoolClientId:   '',         // e.g. fq5t0tnju3b9c14phit21ftn8
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_4DORmL4b9',         // e.g. us-east-2_4DORmL4b9
+        userPoolClientId:   'fq5t0tnju3b9c14phit21ftn8',         // e.g. fq5t0tnju3b9c14phit21ftn8
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
