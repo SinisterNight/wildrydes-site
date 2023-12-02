9d70abd143c43498ffd60581508461eb6a10e605
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-12-02 17:54:03 -0600

    Update scripty.js

--- js/scripty.js
+++ js/scripty.js
@@ -1,22 +1,47 @@
 
 
-function verifyAnswer() {
-  const userAnswer = document.getElementById('verificationAnswer').value;
-  const correctAnswer = '4'; // The correct answer
+// Alert on page opening
+alert('Before you can view the site, complete the captcha.');
+
+// Open a new window with the captcha overlay
+const captchaWindow = window.open('', 'Captcha Window', 'width=400,height=300');
+
+// Display the captcha image with the link you provided
+const captchaImageSrc = 'https://lifewithoutmychild.com/wp-content/uploads/2018/08/IMG_4691-e1546714656195.jpg';
+captchaWindow.document.write(`
+  <div id="captcha-overlay" class="captcha-overlay">
+    <div class="captcha-container">
+      <h2>Complete the Captcha to Access the Site</h2>
+      <img src="${captchaImageSrc}" alt="Captcha Image">
+      <input type="text" id="captchaInput" placeholder="Enter the code">
+      <button onclick="verifyCaptcha()">Submit</button>
+    </div>
+  </div>
+`);
+
+// Chance rate of the captcha (25%)
+const randomChance = Math.random();
+if (randomChance > 0.25) {
+  closeCaptchaOverlay();
+}
+
+// Function to verify the captcha
+function verifyCaptcha() {
+  const userAnswer = captchaWindow.document.getElementById('captchaInput').value;
+  const correctAnswer = 'your_correct_captcha_code'; // Replace with your actual correct answer
 
   if (userAnswer === correctAnswer) {
-    displayVerificationResult('Congratulations! Your answer is correct.');
-   
+    // Correct captcha
+    alert('Congratulations! Your answer is correct.');
+    closeCaptchaOverlay();
   } else {
-    displayVerificationResult('Verification failed. Please try again with the correct answer.');
-   
+    // Incorrect captcha
+    alert('Incorrect answer. Please try again.');
+    captchaWindow.document.getElementById('captchaInput').value = ''; // Reset input
   }
 }
 
-function displayVerificationResult(message) {
-  const resultContainer = document.getElementById('verificationResult');
-  resultContainer.textContent = message;
-  setTimeout(() => {
-    resultContainer.textContent = ''; 
-  }, 3000); 
+// Function to close the captcha overlay
+function closeCaptchaOverlay() {
+  captchaWindow.close();
 }
